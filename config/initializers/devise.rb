5b3890fd2ba784b6493ccc56322caed617740ee5
Kaustubh Kabra <kaustubh.kabra@praxify.com>
2017-01-31 01:45:05 +0530

    All working except carts, filters - commented for now

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,1 +1,11 @@
 Devise.secret_key = "82dd2010a42baf0ac8323d550563fc07f354e987c82ca3dfe804b6c3428c389cd3f839eab4ae9580ad53a7b7a51cf4d8cee9"
+
+Devise.setup do |config|
+    # Required so users don't lose their carts when they need to confirm.
+    config.allow_unconfirmed_access_for = 10.days
+    
+    # Fixes the bug where Confirmation errors result in a broken page.
+    config.router_name = :spree
+    
+    # Add any other devise configurations here, as they will override the defaults provided by spree_auth_devise.
+end
--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -15,4 +15,30 @@ Spree.config do |config|
   # config.track_inventory_levels = false
 end
 
+attachment_config = {
+    s3_credentials: {
+        access_key_id: ENV['AWS_ACCESS_KEY'],
+        secret_access_key: ENV['AWS_SECRET_TOKEN'],
+        bucket: ENV['AWS_BUCKET']
+    },
+    
+    storage:        :s3,
+    s3_protocol:    "https"
+}
+
+if Rails.env.production? || Rails.env.staging?
+    Paperclip::Attachment.default_options[:s3_protocol] = "https"
+    
+    attachment_config.each do |key, value|
+        Spree::Image.attachment_definitions[:attachment][key.to_sym] = value
+        Spree::Taxon.attachment_definitions[:icon][key.to_sym] = value
+        Spree::Banner.attachment_definitions[:image][key.to_sym] = value
+    end
+end
+
+
+Spree::Image.attachment_definitions[:attachment][:path] = '/spree/products/:id/:style/:basename.:extension'
+Spree::Taxon.attachment_definitions[:icon][:path] = '/spree/products/:id/:style/:basename.:extension'
+Spree::Banner.attachment_definitions[:image][:path] = '/spree/banners/:id/:style/:basename.:extension'
+Spree::Auth::Config[:confirmable] = false
 Spree.user_class = "Spree::User"
