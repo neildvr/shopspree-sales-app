e157f042d53cda51f97117f7775a66f509a6a69e
Kaustubh Kabra <kaustubh.kabra@praxify.com>
2017-01-31 12:57:20 +0530

    AWS changes for heroku

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -17,9 +17,9 @@ end
 
 attachment_config = {
     s3_credentials: {
-        access_key_id: ENV['AWS_ACCESS_KEY'],
-        secret_access_key: ENV['AWS_SECRET_TOKEN'],
-        bucket: ENV['AWS_BUCKET']
+        access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+        secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+        bucket: ENV['S3_BUCKET_NAME']
     },
     
     storage:        :s3,
